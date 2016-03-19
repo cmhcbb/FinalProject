b94409482b12af535572523f66e75bc811c0ce64
Minhao Cheng <cmhcbb@gmail.com>
2016-03-18 17:55:21 -0700

    Added files via upload

--- finalprotest.m
+++ finalprotest.m
@@ -1,14 +1,16 @@
-load example1
-F=f_min:7900000:f_max;
+load example2
+a=9:2/1000:11;
+F=10.^a;
 s=2*pi*1i*F;
 s0=f_max;
 R=-(A-s0*E)\B;
 M=(A-s0*E)\E;
-Zk=zeros(2,1001);
+Zk=zeros(16,1001);
+k=130;
 for i=1:1001
-[V,H] = arnoldi(A,R(:,1),150,s0,E);
-e1=zeros(150,1);
+[V,H] = arnoldi(A,R(:,5),k,s0,E);
+e1=zeros(k,1);
 e1(1)=1;
-Zk(:,i)=norm(R(:,1))*C'*V*inv(eye(150)-(s(i)-s0)*H)*e1;
+Zk(:,i)=norm(R(:,5))*C'*V*((eye(k)-(s(i)-s0)*H)\e1);
 i
 end
